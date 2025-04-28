255e64d2cf2d4a789c4407ec02894183b9c89249
evelynsanches <evelyn.eps@hotmail.com>
2025-04-28 21:54:37 +0100

    integrando backend, registro no banco de dados, utilizacao do docker, add listagem de ranking, cadastros utilizando link de referral

--- src/app/invite/[subscriberId]/page.tsx
+++ src/app/invite/[subscriberId]/page.tsx
@@ -1,11 +1,20 @@
 import Image from "next/image"
-import logo from "../../assets/Logo.svg"
+import logo from "../../../assets/Logo.svg"
 import { InviteLinkInput } from "./invite-link-input"
 import { Ranking } from "./ranking"
 import { Stats } from "./stats"
 
-export default function InvitePage() {
-  const inviteLink = "http://localhost:3000/invite/354343434"
+interface InvitePageProps {
+  params: Promise<{
+    subscriberId: string
+  }>
+}
+
+export default async function InvitePage(props: InvitePageProps) {
+  const { subscriberId } = await props.params
+
+  const inviteLink = `http://localhost:3333/invites/${subscriberId}`
+
   return (
     <div className="min-h-dvh flex items-center justify-between gap-16 flex-col md:flex-row">
       <div className="flex flex-col gap-10 w-full max-w-[550px]">
@@ -34,7 +43,7 @@ export default function InvitePage() {
 
           <InviteLinkInput inviteLink={inviteLink} />
 
-          <Stats />
+          <Stats subscriberId={subscriberId} />
         </div>
       </div>
       <Ranking />
